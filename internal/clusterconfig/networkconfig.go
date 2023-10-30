8f31a493dac907e7169b9eeb9e8dc8329a710e05
Igal Tsoiref <itsoiref@redhat.com>
2023-10-30 17:04:25 +0200

    Adding ip validation for primary ip file in order to fail early in case primary ip file will be changed in future Adding internal folder to unitests Adding new unitests

--- internal/clusterconfig/networkconfig.go
+++ internal/clusterconfig/networkconfig.go
@@ -2,24 +2,33 @@ package clusterconfig
 
 import (
 	"context"
+	"fmt"
+	"net"
 	"os"
 	"path/filepath"
 
 	"github.com/go-logr/logr"
 	cp "github.com/otiai10/copy"
+	"github.com/pkg/errors"
 )
 
-const networkDir = "/opt/openshift/network-configuration"
+const (
+	networkDir    = "/opt/openshift/network-configuration"
+	ipAddressFile = "/var/run/nodeip-configuration/primary-ip"
+)
 
 // UpgradeNetworkConfigGather Gather network config files from host
 type UpgradeNetworkConfigGather struct {
 	Log logr.Logger
 }
 
+var hostPath = "/host"
+
 var listOfPaths = []string{
-	"/host/etc/hostname",
-	"/host/etc/NetworkManager/system-connections",
-	"/host/var/run/nodeip-configuration/primary-ip"}
+	"/etc/hostname",
+	"/etc/NetworkManager/system-connections",
+	ipAddressFile,
+}
 
 // FetchNetworkConfig gather network files and copy them
 func (r *UpgradeNetworkConfigGather) FetchNetworkConfig(ctx context.Context, ostreeDir string) error {
@@ -29,9 +38,14 @@ func (r *UpgradeNetworkConfigGather) FetchNetworkConfig(ctx context.Context, ost
 		return err
 	}
 
+	err = r.validateIPAddressFile()
+	if err != nil {
+		return err
+	}
+
 	for _, path := range listOfPaths {
 		r.Log.Info("Copying network files", "file", path, "to", dir)
-		err = cp.Copy(path, filepath.Join(dir, filepath.Base(path)))
+		err = cp.Copy(filepath.Join(hostPath, path), filepath.Join(dir, filepath.Base(path)))
 		if err != nil {
 			return err
 		}
@@ -40,8 +54,22 @@ func (r *UpgradeNetworkConfigGather) FetchNetworkConfig(ctx context.Context, ost
 	return nil
 }
 
-// configDir returns the files directory for the given cluster config
+// validateIpAddressFile validates ip file has valid ip
+func (r *UpgradeNetworkConfigGather) validateIPAddressFile() error {
+	filePath := filepath.Join(hostPath, ipAddressFile)
+	hostIP, err := os.ReadFile(filePath)
+	if err != nil {
+		return errors.Wrapf(err, "Failed to read ip from file %s", filePath)
+	}
 
+	ip := net.ParseIP(string(hostIP))
+	if ip == nil {
+		return fmt.Errorf("failed to parse ip %s from file %s", string(hostIP), filePath)
+	}
+	return err
+}
+
+// configDir returns the files directory for the given cluster config
 func (r *UpgradeNetworkConfigGather) configDir(dir string) (string, error) {
 	filesDir := filepath.Join(dir, networkDir)
 	if err := os.MkdirAll(filesDir, 0o700); err != nil {
