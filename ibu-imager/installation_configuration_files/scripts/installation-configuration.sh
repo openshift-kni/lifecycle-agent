a27dd9c965f0da4af223c725f0a01eb6e44c653e
Don Penney <dpenney@redhat.com>
2023-11-03 15:47:44 -0400

    Update recert cmd args
    Added args to the recert command to allow the recert tool to update the
    /etc/mcs-machine-config-content.json file.
    
    Also added --replace option to the podman run commands.
    
    Signed-off-by: Don Penney <dpenney@redhat.com>

--- ibu-imager/installation_configuration_files/scripts/installation-configuration.sh
+++ ibu-imager/installation_configuration_files/scripts/installation-configuration.sh
@@ -53,8 +53,8 @@ function recert {
     ETCD_IMAGE="$(jq -r '.spec.containers[] | select(.name == "etcd") | .image' </etc/kubernetes/manifests/etcd-pod.yaml)"
     local recert_tool_image=${RECERT_IMAGE:-quay.io/edge-infrastructure/recert:latest}
     local certs_dir=/var/opt/openshift/certs
-    local recert_cmd="sudo podman run --name recert --network=host --privileged -v /var/opt/openshift:/var/opt/openshift -v /etc/kubernetes:/kubernetes -v /var/lib/kubelet:/kubelet -v /etc/machine-config-daemon:/machine-config-daemon ${recert_tool_image} --etcd-endpoint localhost:2379 --static-dir /kubernetes --static-dir /kubelet --static-dir /machine-config-daemon --extend-expiration"
-    sudo podman run --authfile=/var/lib/kubelet/config.json --name recert_etcd --detach --rm --network=host --privileged --entrypoint etcd -v /var/lib/etcd:/store ${ETCD_IMAGE} --name editor --data-dir /store
+    local recert_cmd="sudo podman run --name recert --network=host --privileged --replace -v /var/opt/openshift:/var/opt/openshift -v /etc/kubernetes:/kubernetes -v /var/lib/kubelet:/kubelet -v /etc/machine-config-daemon:/machine-config-daemon -v /etc:/host-etc ${recert_tool_image} --etcd-endpoint localhost:2379 --static-dir /kubernetes --static-dir /kubelet --static-dir /machine-config-daemon --static-file /host-etc/mcs-machine-config-content.json --extend-expiration"
+    sudo podman run --authfile=/var/lib/kubelet/config.json --name recert_etcd --detach --rm --network=host --replace --privileged --entrypoint etcd -v /var/lib/etcd:/store ${ETCD_IMAGE} --name editor --data-dir /store
     sleep 10 # TODO: wait for etcd
 
     # Recert node ip in order to support single ip
