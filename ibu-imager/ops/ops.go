0e436a93ec8fe118d6ab2e21e25dcc34d65eacdc
Leo Ochoa <lochoa@redhat.com>
2023-11-06 23:03:59 +0100

    [imager] Add WaitForEtcd function for recert tool
    Signed-off-by: Leonardo Ochoa-Aday <lochoa@redhat.com>

--- ibu-imager/ops/ops.go
+++ ibu-imager/ops/ops.go
@@ -2,9 +2,18 @@ package ops
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 	"strings"
+	"time"
 
 	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	// Default location for etcdStaticPodFile
+	etcdStaticPodFile = "/etc/kubernetes/manifests/etcd-pod.yaml"
 )
 
 // Ops is an interface for executing commands and actions in the host namespace
@@ -14,6 +23,8 @@ type Ops interface {
 	SystemctlAction(action string, args ...string) (string, error)
 	RunInHostNamespace(command string, args ...string) (string, error)
 	RunBashInHostNamespace(command string, args ...string) (string, error)
+	WaitForEtcd(endpoint string) error
+	GetImageFromPodDefinition(containerImage string) (string, error)
 }
 
 type ops struct {
@@ -66,4 +77,79 @@ func (o *ops) RunInHostNamespace(command string, args ...string) (string, error)
 func (o *ops) RunBashInHostNamespace(command string, args ...string) (string, error) {
 	args = append([]string{command}, args...)
 	return o.RunInHostNamespace("bash", "-c", strings.Join(args, " "))
+}
+
+// WaitForEtcd waits for the availability of the etcd server at the given healthz endpoint.
+// It continuously checks the healthz endpoint to determine the server's availability.
+// If the etcd server becomes accessible, it returns nil, indicating success. Otherwise, it returns an error.
+func (o *ops) WaitForEtcd(healthzEndpoint string) error {
+	// Use the default HTTP client
+	client := http.DefaultClient
+
+	for {
+		// Send an HTTP GET request to the healthz endpoint
+		resp, err := client.Get(healthzEndpoint)
+		if err != nil {
+			// If there was an error, wait for a short interval before retrying
+			time.Sleep(1 * time.Second)
+			o.log.Info(err)
+			continue
+		}
+
+		// Ensure the response body is closed after reading
+		defer resp.Body.Close()
+
+		// If the server responds with HTTP status 200 (OK), it's up and running
+		if resp.StatusCode == http.StatusOK {
+			return nil
+		}
+
+		// Wait for a short interval before retrying
+		time.Sleep(1 * time.Second)
+	}
+}
+
+// GetImageFromPodDefinition reads a YAML file containing pod configuration data
+// and extracts the image name for a given container named.
+// It returns the image name if found, or an error if not found or encountered any issues.
+func (o *ops) GetImageFromPodDefinition(containerImage string) (string, error) {
+
+	// Define a struct to match the structure of the YAML data
+	type PodConfig struct {
+		APIVersion string `yaml:"apiVersion"`
+		Kind       string `yaml:"kind"`
+		Metadata   struct {
+			Name string `yaml:"name"`
+		} `yaml:"metadata"`
+		Spec struct {
+			Containers []struct {
+				Name  string `yaml:"name"`
+				Image string `yaml:"image"`
+			} `yaml:"containers"`
+		} `yaml:"spec"`
+	}
+
+	// Read the YAML file
+	yamlData, err := os.ReadFile(etcdStaticPodFile)
+	if err != nil {
+		return "", fmt.Errorf("error reading the YAML file: %w", err)
+	}
+
+	// Unmarshal the YAML data into the struct
+	var podConfig PodConfig
+	if err = yaml.Unmarshal(yamlData, &podConfig); err != nil {
+		return "", fmt.Errorf("error unmarshaling YAML data: %w", err)
+	}
+
+	// Loop through the containers and find the one named "etcd"
+	var etcdImage string
+	for _, container := range podConfig.Spec.Containers {
+		if container.Name == containerImage {
+			etcdImage = container.Image
+			return etcdImage, nil
+		}
+	}
+
+	// Return an error if no "etcd" container found or no image specified in YAML definition.
+	return "", fmt.Errorf("no 'etcd' container found or no image specified in YAML definition: %w", err)
 }
