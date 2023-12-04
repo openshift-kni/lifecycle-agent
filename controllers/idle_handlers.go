06191c29a53d01dc5cfbee0d0988af84d0be2c9e
Saeid Askari <saskari@redhat.com>
2023-12-04 11:36:14 -0500

    Add handleFinalize and perform backup cleanups
    - Add function to remove all unbooted stateroots
    - Try to run all cleanup functions even if one of them returns an error
    - Run backup cleanup and delete OADP operator in abort and finalize
    
    Signed-off-by: Saeid Askari <saskari@redhat.com>

--- controllers/idle_handlers.go
+++ controllers/idle_handlers.go
@@ -22,12 +22,136 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/openshift-kni/lifecycle-agent/ibu-imager/ops"
+	"github.com/openshift-kni/lifecycle-agent/internal/backuprestore"
 	"github.com/openshift-kni/lifecycle-agent/internal/common"
 
 	lcav1alpha1 "github.com/openshift-kni/lifecycle-agent/api/v1alpha1"
 	"github.com/openshift-kni/lifecycle-agent/controllers/utils"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
+
+func (r *ImageBasedUpgradeReconciler) handleAbort(ctx context.Context, ibu *lcav1alpha1.ImageBasedUpgrade) (ctrl.Result, error) {
+	r.Log.Info("Starting handleAbort")
+
+	if r.cleanup(ctx, false, ibu) {
+		r.Log.Info("Finished handleAbort")
+		return doNotRequeue(), nil
+	}
+	return requeueWithError(fmt.Errorf("failed handelAbort"))
+}
+
+//nolint:unparam
+func (r *ImageBasedUpgradeReconciler) handleAbortFailure(ctx context.Context, ibu *lcav1alpha1.ImageBasedUpgrade) (ctrl.Result, error) {
+	return doNotRequeue(), nil
+}
+
+func (r *ImageBasedUpgradeReconciler) handleFinalize(ctx context.Context, ibu *lcav1alpha1.ImageBasedUpgrade) (ctrl.Result, error) {
+	r.Log.Info("Starting handleFinalize")
+
+	if r.cleanup(ctx, true, ibu) {
+		r.Log.Info("Finished handleFinalize")
+		return doNotRequeue(), nil
+	}
+	return requeueWithError(fmt.Errorf("failed handleFinalize"))
+}
+
+//nolint:unparam
+func (r *ImageBasedUpgradeReconciler) handleFinalizeFailure(ctx context.Context, ibu *lcav1alpha1.ImageBasedUpgrade) (ctrl.Result, error) {
+
+	// TODO actual steps
+	// If succeeds, return doNotRequeue
+	return doNotRequeue(), nil
+}
+
+// cleanup cleans stateroots, precache, backup, ibu files and OADP operator
+// returns true if all cleanup tasks were successful
+func (r *ImageBasedUpgradeReconciler) cleanup(
+	ctx context.Context, allUnbootedStateroots bool, ibu *lcav1alpha1.ImageBasedUpgrade) bool {
+	// try to clean up as much as possible and avoid returning when one of the cleanup tasks fails
+	// successful means that all the cleanup tasks completed without any error
+	successful := true
+
+	if err := r.cleanupStateroots(allUnbootedStateroots, ibu); err != nil {
+		successful = false
+		r.Log.Error(err, "failed to cleanup stateroots")
+	}
+	if err := r.Precache.Cleanup(ctx); err != nil {
+		successful = false
+		r.Log.Error(err, "failed to cleanup precaching resources")
+	}
+	if _, err := r.BackupRestore.CleanupBackups(ctx); err != nil {
+		successful = false
+		r.Log.Error(err, "failed to cleanup backups")
+	}
+	if err := r.BackupRestore.DeleteOadpOperator(ctx, backuprestore.OadpNs); err != nil {
+		successful = false
+		r.Log.Error(err, "failed to delete OADP operator")
+	}
+	if err := cleanupIBUFiles(); err != nil {
+		successful = false
+		r.Log.Error(err, "failed to cleanup ibu files")
+	}
+
+	return successful
+}
+
+// cleanupStateroot cleans all unbooted stateroots or desired stateroot
+// depending on allUnbootedStateroots argumennt
+func (r *ImageBasedUpgradeReconciler) cleanupStateroots(
+	allUnbootedStateroots bool, ibu *lcav1alpha1.ImageBasedUpgrade) error {
+	if allUnbootedStateroots {
+		return r.cleanupUnbootedStateroots()
+	}
+	return r.cleanupUnbootedStateroot(getDesiredStaterootName(ibu))
+}
+
+func cleanupIBUFiles() error {
+	if _, err := os.Stat(common.PathOutsideChroot(utils.IBUWorkspacePath)); err != nil {
+		return nil
+	}
+	if err := os.RemoveAll(filepath.Join(utils.Host, utils.IBUWorkspacePath)); err != nil {
+		return fmt.Errorf("removing %s failed: %w", utils.IBUWorkspacePath, err)
+	}
+	return nil
+}
+
+func (r *ImageBasedUpgradeReconciler) cleanupUnbootedStateroots() error {
+	status, err := r.RPMOstreeClient.QueryStatus()
+	if err != nil {
+		return err
+	}
+
+	undeployIndices := make([]int, 0)
+	undeployStateroots := make([]string, 0)
+	for index, deployment := range status.Deployments {
+		if deployment.Booted {
+			continue
+		}
+		undeployIndices = append(undeployIndices, index)
+		undeployStateroots = append(undeployStateroots, deployment.OSName)
+	}
+	// since undeploy shifts the order, undeploy in the reverse order
+	for i := len(undeployIndices) - 1; i >= 0; i-- {
+		_, err = r.Executor.Execute("ostree", "admin", "undeploy", fmt.Sprint(i))
+		if err != nil {
+			return fmt.Errorf("ostree undeploy %d failed: %w", i, err)
+		}
+	}
+
+	failures := 0
+	for _, stateroot := range undeployStateroots {
+		if err = removeStateroot(r.Executor, stateroot); err != nil {
+			r.Log.Error(err, "failed to remove stateroot", "stateroot", stateroot)
+			failures += 1
+		}
+	}
+
+	if failures == 0 {
+		return nil
+	}
+	return fmt.Errorf("failed to remove %d stateroots", failures)
+}
 
 func (r *ImageBasedUpgradeReconciler) cleanupUnbootedStateroot(stateroot string) error {
 	status, err := r.RPMOstreeClient.QueryStatus()
@@ -54,67 +178,18 @@ func (r *ImageBasedUpgradeReconciler) cleanupUnbootedStateroot(stateroot string)
 		}
 	}
 
-	// remove stateroot
-	sysrootPath := fmt.Sprintf("/sysroot/ostree/deploy/%s", stateroot)
-	if _, err := os.Stat(common.PathOutsideChroot(sysrootPath)); err == nil {
-		_, err = r.Executor.Execute("unshare", "-m", "/bin/sh", "-c",
-			fmt.Sprintf("\"mount -o remount,rw /sysroot && rm -rf %s\"", sysrootPath))
-		if err != nil {
-			return fmt.Errorf("removing stateroot failed: %w", err)
-		}
+	return removeStateroot(r.Executor, stateroot)
+}
+
+func removeStateroot(executor ops.Execute, stateroot string) error {
+	staterootPath := fmt.Sprintf("/sysroot/ostree/deploy/%s", stateroot)
+	if _, err := os.Stat(common.PathOutsideChroot(staterootPath)); err != nil {
+		return nil
 	}
 
-	return nil
-}
-
-//nolint:unparam
-func (r *ImageBasedUpgradeReconciler) handleAbort(ctx context.Context, ibu *lcav1alpha1.ImageBasedUpgrade) (ctrl.Result, error) {
-	// Cleanup precaching resources
-	r.Log.Info("Cleanup precaching resources")
-	if err := r.Precache.Cleanup(ctx); err != nil {
-		r.Log.Info("Failed to cleanup precaching resources")
-		return requeueWithError(err)
-	}
-
-	stateroot := getDesiredStaterootName(ibu)
-	r.Log.Info("Cleanup stateroot", "stateroot", stateroot)
-	err := r.cleanupUnbootedStateroot(stateroot)
-	if err != nil {
-		return requeueWithError(err)
-	}
-	err = cleanupIBUFiles()
-	return requeueWithError(err)
-}
-
-//nolint:unparam
-func (r *ImageBasedUpgradeReconciler) handleAbortFailure(ctx context.Context, ibu *lcav1alpha1.ImageBasedUpgrade) (ctrl.Result, error) {
-
-	// TODO actual steps
-	// If succeeds, return doNotRequeue
-	return doNotRequeue(), nil
-}
-
-//nolint:unparam
-func (r *ImageBasedUpgradeReconciler) handleFinalize(ctx context.Context, ibu *lcav1alpha1.ImageBasedUpgrade) (ctrl.Result, error) {
-
-	// TODO actual steps
-	// If succeeds, return doNotRequeue
-	return doNotRequeue(), nil
-}
-
-//nolint:unparam
-func (r *ImageBasedUpgradeReconciler) handleFinalizeFailure(ctx context.Context, ibu *lcav1alpha1.ImageBasedUpgrade) (ctrl.Result, error) {
-
-	// TODO actual steps
-	// If succeeds, return doNotRequeue
-	return doNotRequeue(), nil
-}
-
-func cleanupIBUFiles() error {
-	if _, err := os.Stat(common.PathOutsideChroot(utils.IBUWorkspacePath)); err == nil {
-		if err := os.RemoveAll(filepath.Join(utils.Host, utils.IBUWorkspacePath)); err != nil {
-			return fmt.Errorf("removing %s failed: %w", utils.IBUWorkspacePath, err)
-		}
+	if _, err := executor.Execute("unshare", "-m", "/bin/sh", "-c",
+		fmt.Sprintf("\"mount -o remount,rw /sysroot && rm -rf %s\"", staterootPath)); err != nil {
+		return fmt.Errorf("removing stateroot %s failed: %w", stateroot, err)
 	}
 	return nil
 }
