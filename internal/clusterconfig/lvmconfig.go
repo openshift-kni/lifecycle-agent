61463dbb411f394049073e4469ac155732d3f552
Angie Wang <angwang@redhat.com>
2024-03-11 00:02:51 -0400

    Save localvolumes to the manifests dir for post-pivot service to restore

--- internal/clusterconfig/lvmconfig.go
+++ internal/clusterconfig/lvmconfig.go
@@ -23,16 +23,39 @@ import (
 	"path/filepath"
 
 	"github.com/openshift-kni/lifecycle-agent/internal/common"
+	"github.com/openshift-kni/lifecycle-agent/utils"
 	cp "github.com/otiai10/copy"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"k8s.io/apimachinery/pkg/types"
 )
 
+// +kubebuilder:rbac:groups=local.storage.openshift.io,resources=localvolumes,verbs=get;list;watch
+
 func (r *UpgradeClusterConfigGather) FetchLvmConfig(ctx context.Context, ostreeDir string) error {
-	r.Log.Info("Fetching node lvm files")
+	r.Log.Info("Fetching node lvm configuration")
 	lvmConfigPath := filepath.Join(ostreeDir, common.OptOpenshift, common.LvmConfigDir)
 	if err := os.MkdirAll(lvmConfigPath, 0o700); err != nil {
 		return fmt.Errorf("failed to make dir in %s: %w", lvmConfigPath, err)
 	}
 
+	if err := r.fetchLvmDevices(lvmConfigPath); err != nil {
+		return err
+	}
+
+	manifestsDir := filepath.Join(ostreeDir, common.OptOpenshift, common.ClusterConfigDir, manifestDir)
+	if err := r.fetchLocalVolumes(ctx, manifestsDir); err != nil {
+		return err
+	}
+
+	r.Log.Info("Successfully fetched lvm configuration")
+	return nil
+}
+
+func (r *UpgradeClusterConfigGather) fetchLvmDevices(lvmConfigPath string) error {
 	r.Log.Info("Copying lvm devices file", "file", common.LvmDevicesPath, "to", lvmConfigPath)
 	err := cp.Copy(
 		filepath.Join(hostPath, common.LvmDevicesPath),
@@ -44,6 +67,45 @@ func (r *UpgradeClusterConfigGather) FetchLvmConfig(ctx context.Context, ostreeD
 		}
 		return fmt.Errorf("failed to lvm devices file: %w", err)
 	}
-	r.Log.Info("Done fetching node lvm files")
+
+	return nil
+}
+
+func (r *UpgradeClusterConfigGather) fetchLocalVolumes(ctx context.Context, manifestsDir string) error {
+	r.Log.Info("Fetching local volumes")
+
+	crd := &apiextensionsv1.CustomResourceDefinition{}
+	if err := r.Client.Get(ctx, types.NamespacedName{Name: "localvolumes.local.storage.openshift.io"}, crd); err != nil {
+		if k8serrors.IsNotFound(err) {
+			r.Log.Info("LocalVolume CRD is not found. Skipping")
+			return nil
+		}
+		return fmt.Errorf("failed to get LocalVolume CRD: %w", err)
+	}
+
+	// List localvolumes
+	lvsList := &unstructured.UnstructuredList{}
+	lvsList.SetGroupVersionKind(schema.GroupVersionKind{
+		Group:   "local.storage.openshift.io",
+		Kind:    "LocalVolumeList",
+		Version: "v1",
+	})
+	if err := r.List(ctx, lvsList); err != nil {
+		return fmt.Errorf("failed to list localvolumes: %w", err)
+	}
+
+	for _, lv := range lvsList.Items {
+		// Unset uid and resource version
+		lv.SetUID("")
+		lv.SetResourceVersion("")
+
+		filePath := filepath.Join(manifestsDir, lv.GetName()+"_"+lv.GetNamespace()+".json")
+		r.Log.Info("Writing LocalVolume to file", "path", filePath)
+		err := utils.MarshalToFile(lv.Object, filePath)
+		if err != nil {
+			return fmt.Errorf("failed to write localvolume to %s: %w", filePath, err)
+		}
+	}
+
 	return nil
 }
