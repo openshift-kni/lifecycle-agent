d041995867c9f9e184719ad89924165d69bf0306
Don Penney <dpenney@redhat.com>
2024-05-08 08:21:52 -0400

    reset version to 4.15 after resync with main
    Resynced branch against main, up to pull request #493
    
    Signed-off-by: Don Penney <dpenney@redhat.com>

--- internal/common/consts.go
+++ internal/common/consts.go
@@ -38,7 +38,7 @@ const (
 	KubeconfigFile        = "/etc/kubernetes/static-pod-resources/kube-apiserver-certs/secrets/node-kubeconfigs/lb-ext.kubeconfig"
 
 	RecertImageEnvKey      = "RELATED_IMAGE_RECERT_IMAGE"
-	DefaultRecertImage     = "quay.io/edge-infrastructure/recert:v0"
+	DefaultRecertImage     = "quay.io/edge-infrastructure/recert:release-4.15"
 	EtcdStaticPodFile      = "/etc/kubernetes/manifests/etcd-pod.yaml"
 	EtcdStaticPodContainer = "etcd"
 	EtcdDefaultEndpoint    = "localhost:2379"
