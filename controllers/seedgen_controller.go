f3deda357ed98e4f51965b3be8ce7a1c417f50d5
Leo Ochoa <lochoa@redhat.com>
2024-07-02 14:54:15 +0200

    seedgen: Add check to reject when OADP version ain't supported
    Signed-off-by: Leonardo Ochoa-Aday <lochoa@redhat.com>

--- controllers/seedgen_controller.go
+++ controllers/seedgen_controller.go
@@ -640,6 +640,16 @@ func (r *SeedGeneratorReconciler) validateSystem(ctx context.Context) (msg strin
 		return
 	}
 
+	// Ensure the minimum supported version for installed OADP
+	if oadpSupportedVersion, err := r.BackupRestore.CheckOadpMinimumVersion(ctx); !oadpSupportedVersion {
+		if err != nil {
+			msg = fmt.Sprintf("Failure occurred when checking OADP version in system validation: %v", err)
+		} else {
+			msg = fmt.Sprintf("Rejected: Cluster must have OADP version >= %v", backuprestore.OadpMinSupportedVersion)
+		}
+		return
+	}
+
 	return
 }
 
